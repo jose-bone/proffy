bcc19fa4f0a413dcdb254398b528ddc8310b974b
José Boné <79787418+jose-bone@users.noreply.github.com>
2022-03-08 22:30:21 +0200

    Structuring and stylizing the landing page 🛠🎨

--- App.tsx
+++ App.tsx
@@ -2,12 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <>
+        <Landing />
+        <StatusBar style="light" />
+      </>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
