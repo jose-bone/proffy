d8b8bd1afe8b290ecd39b12d1e61ae4246deb22c
José Boné <79787418+jose-bone@users.noreply.github.com>
2022-04-02 11:56:22 +0200

    Contato via WhatsApp 📱

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -22,6 +22,10 @@ interface TeacherItemProps {
 }
 
 export const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function handleLinkToWhatsApp() {
+    Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.profile}>
@@ -47,7 +51,10 @@ export const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
             <Image source={unfavoriteIcon} />
           </RectButton>
 
-          <RectButton style={styles.contactButton}>
+          <RectButton
+            onPress={handleLinkToWhatsApp}
+            style={styles.contactButton}
+          >
             <Image source={whatsAppIcon} />
             <Text style={styles.contactButtonText}>Entrar em contato</Text>
           </RectButton>
