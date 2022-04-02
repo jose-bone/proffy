3d05b10da9e54f3f3c350993a74a4ac03e6371b1
José Boné <79787418+jose-bone@users.noreply.github.com>
2022-04-02 08:41:35 +0200

    Lista de Favoritos 💜

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -9,6 +9,20 @@ export function Favorites() {
   return (
     <View style={styles.container}>
       <PageHeader title="Meus proffys favoritos" />
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   );
 }
